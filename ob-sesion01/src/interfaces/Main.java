0db15828d6b4a75b22da01d3542290d4fcea617c
ADRIANMEDINALIMON <adrian98business@gmail.com>
2022-10-19 11:37:27 -0500

    update: sesion 789 entrega ejercicios

--- ob-sesion01/src/interfaces/Main.java
+++ ob-sesion01/src/interfaces/Main.java
@@ -4,8 +4,20 @@ public interface Main {
 
 	public static void main(String[] args) {
 		
-		
-		|
+		String cadena = "Mensaje de texto";
+		char primera = cadena.charAt(0);
+		char ultima = cadena.charAt(14);
+
+
+
+		for ( int i = 1; i < cadena.length() -1; i++ ) {
+			System.out.println("el resulado es " + cadena.charAt(i) );
+
+
+
+
+		}
+
 	}
 	
 	
